a80f3fae1de3718fed74953775b2a2dbcdf16529
Proddutur Shruthi <50660722+shruthiamar@users.noreply.github.com>
2021-10-19 14:42:38 +0530

    Add files via upload

---
+++ Shruthi Proddutur/BiangBiang.java
@@ -0,0 +1,11 @@
+package ParentChild;
+
+class BiangBiang extends Noodle {
+
+    BiangBiang() {
+
+        super(50.0, 5.0, "flat", "high-gluten flour, salt, water");
+
+    }
+
+}
---
+++ Shruthi Proddutur/Noodle.java
@@ -0,0 +1,45 @@
+package ParentChild;
+
+public class Noodle {
+    protected double lengthInCentimeters;
+    protected double widthInCentimeters;
+    protected String shape;
+    protected String ingredients;
+    protected String texture = "brittle";
+
+    Noodle(double lenInCent, double wthInCent, String shp, String ingr) {
+
+        this.lengthInCentimeters = lenInCent;
+        this.widthInCentimeters = wthInCent;
+        this.shape = shp;
+        this.ingredients = ingr;
+
+    }
+
+    public void cook() {
+
+        this.texture = "cooked";
+
+    }
+
+    public String getCookPrep() {
+
+        return "Boil noodle for 7 minutes and add sauce.";
+
+    }
+    public static void main(String[] args) {
+
+        Noodle spaghetti, ramen, pho;
+
+        spaghetti = new Spaghetti();
+        ramen = new Ramen();
+        pho = new Pho();
+
+        // child class in arrays or arraylist:
+        Noodle[] allTheNoodles = {spaghetti,ramen,pho};
+        for(Noodle allTheNoodle:allTheNoodles){
+            System.out.println(allTheNoodle.getCookPrep());
+        }
+
+    }
+}
